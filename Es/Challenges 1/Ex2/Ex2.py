11dcb29c40537f64390f535ac7d6173785ff7112
Solgio <lorystar314@gmail.com>
2024-10-07 09:13:20 +0200

    After confrontation with given solutions

--- Es/Challenges 1/Ex2/Ex2.py
+++ Es/Challenges 1/Ex2/Ex2.py
@@ -5,25 +5,62 @@ operator=input()
 print("Inserisci il secondo numero: ")
 input2=input()
 
+i1=int(input1)
+i2=int(input2)
 match operator:
     case "+":
-        print( input1," + ", input2, " = ", int(input1)+int(input2))
+        print( f"{i1} + {i2} = {i1 + 12}")
         
     case "-":
-        print( input1," - ", input2, " = ", int(input1)-int(input2))
-         
+        print( f"{i1} - {i2} = {i1 - 12}") 
+
     case "*":
-        print( input1," * ", input2, " = ", int(input1)*int(input2))
-         
+        print( f"{i1} * {i2} = {i1 * 12}")
+
     case "/":
-        print( input1," / ", input2, " = ", int(input1)/int(input2))
-    
+        print( f"{i1} / {i2} = {i1 / 12}")
+
     case "%":
-        print( input1," % ", input2, " = ", int(input1)%int(input2))
-        
+        print( f"{i1} % {i2} = {i1 % 12}")
+
     case "^":
-        print( input1," ^ ", input2, " = ", int(input1)**int(input2))
-        
+        print( f"{i1} ^ {i2} = {i1 ** 12}")
+
     case "rad":
-        print( "rad(", input2, ")", "di ", input1, " = ", int(input1)**(1/int(input2)))
-        
+        print( f"rad({i2}) di {i1} = { i1**(1/i2)}")
+
+
+
+'''Other solution with Parser
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--x1", type = int)
+parser.add_argument("--x2", type = int)
+parser.add_argument("--y", type = int)
+args = parser.parse_args()
+
+
+def main():
+    #get the arguments
+    x1 = args.x1
+    x2 = args.x2
+    operation = args.y
+
+    #adding f before the " allows you to insert variables
+    #inside the string
+    print(f"x1={x1}\tx2={x2}")
+
+    if operation == 0:
+        print(f"solution={x1 + x2}")
+    elif operation == 1:
+        print(f"solution={x1 - x2}")
+    elif operation == 2:
+        print(f"solution={x1 * x2}")
+    if operation == 3:
+        print(f"solution={x1 / x2}")
+
+
+if __name__ == '__main__':
+    #usage example: python solution.py --x1 1 --x2 2 --y 3
+    main()
+'''
--- Es/Challenges 1/Ex3/Ex3.py
+++ Es/Challenges 1/Ex3/Ex3.py
@@ -1,11 +1,10 @@
 import random
 
 password=[]
-i=0
+
 for i in range (10):
     num=(random.randint(33,126))
     password.append(chr(num))
-    i+=1
 
     
 password=''.join(password)
